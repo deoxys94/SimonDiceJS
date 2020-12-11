a03ebecf6156cf8e48cbf83f766f17beeb38680a
deoxys94 <deoxys94@gmail.com>
2020-12-11 11:24:55 +0800

    Terminado juego: 1. Agregado un modal usando SweetAlert 2. Terminada la lógica y jugabilidad básica 3. Agregados comentarios para comprender mejor el código

--- juego.js
+++ juego.js
@@ -5,35 +5,59 @@ const botonMorado = document.getElementById('morado');
 const botonNaranja = document.getElementById('naranja');
 const botonVerde = document.getElementById('verde');
 const botonEmpezar = document.getElementById('botonEmpezar');
+const ultimoNivel = 10;
 let juego;
 
 class Juego 
 {
     constructor()
-    {
-        //#region Inicializar
-        // Ocular el botón de inicio
-        botonEmpezar.classList.add('hide');
-        // Generar datos del juego
-        this.nivel = 10;
-        this.colores = {azul, morado, naranja, verde};
-
-        // generar secuencia de números
-        
-        //#endregion
-    
-        this.generarSecuencia();
-        this.siguienteNivel();
+    { 
+        this.iniciarJuego = this.iniciarJuego.bind(this); // Hace que this apunte al mismo objeto, siempre
+        this.iniciarJuego();
+        this.generarSecuencia(); // Genera la secuencia de números necesaria para jugar
+        setTimeout(() => {
+            this.siguienteNivel();
+        }, 500); // El juego inicia en el nivel "0". Esta linea "ayuda" a que pase al nivel 1
     }
 
+    iniciarJuego()
+    {
+        //#region Inicializar
+
+        // Hacer que el puntero this sea lo mismo dentro de toda la clase
+        this.elegirColor = this.elegirColor.bind(this);
+        this.siguienteNivel = this.siguienteNivel.bind(this);
+
+        // Ocular el botón de inicio
+        this.estadoBotonEmpezar();
+        
+        // Generar datos del juego
+        this.nivel = 1;
+        this.colores = {azul, morado, naranja, verde};
+        
+        //#endregion
+    }
+
+    // Muestra u oculta el botón de inicio
+    estadoBotonEmpezar()
+    {
+        if(botonEmpezar.classList.contains('hide'))
+            botonEmpezar.classList.remove('hide');
+        else
+            botonEmpezar.classList.add('hide');
+    }
+
+    // Generar la secuencia de números necesaria para el juego
     generarSecuencia()
     {
-        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+        this.secuencia = new Array(ultimoNivel).fill(0).map(n => Math.floor(Math.random() * 4));
     }
 
     siguienteNivel()
     {
+        this.subNivel = 0; // Variable necesaria para "recordar" el input del usuario
         this.iluminarSecuencia();
+        this.inputUsuario();
     }
 
     numeroAColor(numero)
@@ -50,11 +74,25 @@ class Juego
         }
     }
 
+    colorANumero(color)
+    {
+        switch (color) 
+        {
+            case 'azul':
+                return 0;
+            case 'morado':
+                return 1;
+            case 'naranja':
+                return 2;
+            case 'verde':
+                return 3;
+        }
+    }
+
     iluminarSecuencia()
     {
         for(let i = 0; i < this.nivel; i++)
         {
-            console.log(i);
             let color = this.numeroAColor(this.secuencia[i]);
 
             setTimeout(() => {
@@ -72,6 +110,94 @@ class Juego
     apagarColor(color)
     {
         this.colores[color].classList.remove('light');
+    }
+
+    // Permite que el navegador registre los clicks del usuario    
+    inputUsuario()
+    {
+        this.colores.azul.addEventListener('click', this.elegirColor);
+        this.colores.morado.addEventListener('click', this.elegirColor);
+        this.colores.naranja.addEventListener('click', this.elegirColor);
+        this.colores.verde.addEventListener('click', this.elegirColor);
+    }
+
+    // Elimina los event listeners para pasar al siguiente nivel
+    restringirInputUsuario()
+    {
+        this.colores.azul.removeEventListener('click', this.elegirColor);
+        this.colores.morado.removeEventListener('click', this.elegirColor);
+        this.colores.naranja.removeEventListener('click', this.elegirColor);
+        this.colores.verde.removeEventListener('click', this.elegirColor);
+    }
+
+    ganarJuego()
+    {
+        Swal.fire(
+            'Buen trabajo!!',
+            'Ganaste!!',
+            'success'
+        ).then(() => 
+        {
+            this.iniciarJuego();
+        });
+    }
+
+    perdioJuego()
+    {
+        Swal.fire(
+            'Oh no!!',
+            'Has perdido. Buena suerte para la próxima.',
+            'error').then(() =>
+            {
+                this.restringirInputUsuario();
+                this.iniciarJuego();
+            });
+    }
+
+    /* 
+        Aqui es donde la "magia ocurre" 
+        1. Obtiene el color que el usuario halla seleccionado (via el dataset designado)
+        2. Ilumina el color seleccionado por el usuario
+        3. si el color seleccionado NO es el mismo que el juego espera, el juego termina
+        4. si el color seleccionado es el juego espera:
+            - La variable subnivel (un indice al arreglo de numeros generado al inicio) aumenta 1
+            - Si el subnivel es igual al último nivel + 1, el juego termina
+            - Si el subnivel es igual al nivel actual: 
+                - se eliminan los eventListener.
+                - se incrementa el nivel actual.
+                - se ejecuta siguiente nivel.
+    */
+
+    elegirColor(e)
+    {
+        let nombreColor = e.target.dataset.color;
+        let numeroColor = this.colorANumero(nombreColor);
+        this.iluminarColor(nombreColor);
+
+        if(numeroColor === this.secuencia[this.subNivel])
+        {
+            this.subNivel++;
+
+            if(this.subNivel === this.nivel)
+            {
+                this.nivel++;
+                this.restringirInputUsuario();
+                if (this.nivel === (ultimoNivel + 1))
+                {
+                    this.ganarJuego();
+                }
+                else
+                {
+                    setTimeout(() => {
+                        this.siguienteNivel()
+                    }, 2000);
+                }
+            }
+        }
+        else
+        {
+            this.perdioJuego();
+        }
     }
 }
 
